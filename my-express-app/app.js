a7907299e448f9951946e24ed128f1814ef99964
Firstname Lastname <alexandraachim.calderon@gmail.com>
2021-01-14 21:08:15 +0100

    fix serverless

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -2,12 +2,19 @@
 
 // eslint-disable-next-line import/no-unresolved
 const express = require('express');
+const superagent = require('superagent');
 
 const app = express();
 
 // Routes
-app.get('/*', (req, res) => {
-  res.send(`Request received: ${req.method} - ${req.path}`);
+app.get('/*', async (req, res) => {
+  try { 
+    const {text} = await superagent.get('https://api.exchangeratesapi.io/latest')
+    const finalText = JSON.parse(text);
+    res.status(200).send(finalText);
+    } catch(err) {
+     throw new Error;
+ }
 });
 
 // Error handler
